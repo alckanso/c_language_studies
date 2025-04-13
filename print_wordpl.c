abea58aebbaf5f1c1fb6ef5e612ef9183c7a069e
alckanso <46038080+alckanso@users.noreply.github.com>
2025-04-13 10:53:05 -0300

    Create print_wordpl.c

---
+++ print_wordpl.c
@@ -0,0 +1,41 @@
+/* Program to print input one word per line */
+/* 1) If first input is space or tab, program need to ignore it;
+2) If not, just input, until a space or tab character is input;
+3) On second space or tab input, include newline command, and set "fc" to 1, to avoid double space and blank lines;
+4) Consider "fc" equal 1 for space or tab, so a sequence with "space+tab" or "tab+space", avoid double new lines;
+*/
+
+
+#include <stdio.h>
+
+main()
+{
+
+	int c, h, fc, st; /* c = character; fc = first character ; h = history ; st = spacetab */
+
+	fc = 0;
+	c = getchar();
+	while (c != EOF) {
+		if ((c == ' ' || c == '\t') && (fc == 0)) {
+			h = c;
+			++fc; /* Increment fc to 1 - now we know its not the first input */
+			printf("\n"); /* Include a new line when space or tabs input */
+			c = getchar();
+		}
+		else if ((c == ' ' || c == '\t') && (fc != 0) && (h != ' ' && h != '\t')) {
+			h = c;
+			printf("\n"); /* Include a new line when space or tabs input */
+			c = getchar();
+		}
+		/* next else will ignore space and tabs in sequence */
+		else if ((c == ' ' && h == ' ') || (c == '\t' && h == '\t'))
+			c = getchar();
+		else if ((c == ' ' && h == '\t') || (c == '\t' && h == ' '))
+			c = getchar();
+		else if (c != ' ' && c != '\t') {
+			putchar(c);
+			h = c;
+			c = getchar();
+		}
+	}
+}
